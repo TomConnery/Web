c84846839ae889aed547e8555bf0ceff5a4b5308
TomConnery <l.dapo@gmx.net>
2019-03-30 00:02:22 +0100

    Enabled Gradle support

--- src/main/java/de/tomconn/web/annotation/HasBulk.java
+++ src/main/java/de/tomconn/web/annotation/HasBulk.java
@@ -1,4 +1,4 @@
-package de.tomconn.Web.annotation;
+package de.tomconn.web.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -6,7 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * <p>Methods annotated with this annotation are marked to have no bulk method, meaning that bulk-processing behavior has to be implemented by users.</p>
+ * <p>Methods annotated with this annotation are meant to have a respective bulk-processing pendant.</p>
+ * <p>Named pendant is expected to take a collection of arguments and return a collection of return values</p>
  * <br></br>
  * <p>This is a source-marker annotation and will not be carried over into runtime.</p>
  * <p>It is expected to be used by IDEs to index the proper building process of APIs.</p>
@@ -15,6 +16,6 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.METHOD)
-public @interface Single {
+public @interface HasBulk {
 
 }
--- src/module-info.java
+++
@@ -1,3 +0,0 @@
-module Web {
-    exports de.tomconn.Web.annotation;
-}
